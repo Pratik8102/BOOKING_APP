e88a0f63fcfff06c29f652e04a3434e579c61c77
Pratik8102 <leetcodesharma@gmail.com>
2024-04-12 19:58:38 +0530

    24.37min

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,10 +1,24 @@
 import express from "express";
+import Hotel from "../models/Hotel.js";
+
+
 const router =express.Router();
 
 
 //create
-router.post("/",(req,res)={
+router.post("/", async (req,res)=>{
+   
+    const newHotel=new Hotel(req.body) 
     
+    try{
+       const saveHotel= await newHotel.save()
+       res.status(200).json(saveHotel)
+    }
+    catch(err)
+    {
+        res.status(500).json(err)
+    }
+
 })
 
 export default router
