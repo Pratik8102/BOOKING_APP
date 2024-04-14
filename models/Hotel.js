a3d25e76ababbb7e84d14edeea74e6db9b4d959e
Pratik8102 <leetcodesharma@gmail.com>
2024-04-14 13:37:08 +0530

    31.33

--- models/Hotel.js
+++ models/Hotel.js
@@ -26,6 +26,10 @@ const HotelSchema=new mongoose.Schema({
     photos:{
         type:[String],
     },
+    title:{
+        type:String,
+        required:true
+    },
     desc:{
         type:String,
         required:true
--- routes/hotels.js
+++ routes/hotels.js
@@ -11,14 +11,68 @@ router.post("/", async (req,res)=>{
     const newHotel=new Hotel(req.body) 
     
     try{
-       const saveHotel= await newHotel.save()
-       res.status(200).json(saveHotel)
+       const saveHotel= await newHotel.save();
+       res.status(200).json(saveHotel);
     }
     catch(err)
     {
         res.status(500).json(err)
     }
 
-})
+});
+router.put("/:id", async (req,res)=>{  
+    try{
+       const updatedHotel= await Hotel.findByIdAndUpdate(req.params.id,{$set: req.body},{new:true });
+       res.status(200).json(updatedHotel);
+    }
+    catch(err)
+    {
+        console.log(err)
+        res.status(500).json(err)
+    }
+
+});
+//delete
+router.delete("/:id", async (req,res)=>{  
+    try{
+       await Hotel.findByIdAndDelete(
+        req.params.id
+    );
+       res.status(200).json("Hotel has been deleted");
+    }
+    catch(err)
+    {
+        console.log(err)
+        res.status(500).json(err)
+    }
+
+});
+router.get("/:id", async (req,res)=>{  
+    try{
+       const hotel= await Hotel.findById(
+        req.params.id
+    );
+      res.status(200).json(hotel);
+    }
+    catch(err)
+    {
+        // console.log(err)
+        res.status(500).json(err)
+    }
+
+});
+router.get("/", async (req,res)=>{  
+    try{
+       const hotels= await Hotel.find();
+      res.status(200).json(hotels);
+    }
+    catch(err)
+    {
+        // console.log(err)
+        res.status(500).json(err)
+    }
+
+});
+
 
 export default router
